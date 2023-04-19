7948f7bb4b7251d52821644d047869351cecd788
andreamarangon <marangonandrea84@gmail.com>
2023-04-19 19:13:20 +0200

    chore: slider

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import Head from 'next/head'
 import Hero from '@/components/Hero'
+import Slider from '@/components/Slider'
+import { SliderData } from '@/components/SliderData'
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
         <title>Photography Next App</title>
       </Head>
       <Hero heading="Captur Photography" message="I capture moments in nature and keep them alive." />
+      <Slider slides={SliderData} />
     </div>
   )
 }
