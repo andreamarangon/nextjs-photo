08ba31653d40cbbcc0ddfb7211919d6cae6a9b50
andreamarangon <marangonandrea84@gmail.com>
2023-04-28 16:23:29 +0200

    fix: overlay rounded

--- components/InstagramImg.jsx
+++ components/InstagramImg.jsx
@@ -7,7 +7,7 @@ const InstagramImg = ({ socialImg }) => {
     <div className="relative">
       <Image src={socialImg} alt="/" className="w-full h-full rounded-lg" responsive="true" />
       {/* Overlay */}
-      <div className="flex justify-center items-center absolute top-0 left-0 right-0 bottom-0 hover:bg-black/50 group" >
+      <div className="flex justify-center items-center absolute rounded-lg top-0 left-0 right-0 bottom-0 hover:bg-black/50 group" >
         <p className="text-gray-300 hidden group-hover:block">
           <FaInstagram size={30} className="z-10" />
         </p>
